1a64fa501e01e5b257731d7460cf54ac7c1b0621
Isaac Wanger <teryima127@gmail.com>
2022-12-23 18:48:54 +0100

    second commit

--- main.go
+++ main.go
@@ -1,13 +1,45 @@
 package main
 
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
 type todo struct {
-	ID          string
-	Item        string
-	Completed   bool
+	ID          string `json:"id"`
+	Item        string  `json:"title"`
+	Completed   bool     `json:"completed"`
 }
 
 var todos = []todo {
 	{ID: "1", Item: "Clean Room", Completed: false},
 	{ID: "2", Item: "Read Book", Completed: false},
 	{ID: "1", Item: "Watch Footbal", Completed: false},
+}
+
+/////////////////////////////////////////////
+// Get todos ---GET Method
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
+////////////////////////////////////////////////
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// Add todos ---POST Method
+func addTodo(context *gin.Context) {
+	var newTodo todo
+
+	 err := context.BindJSON(&newTodo)
+	 if err != nil {
+		return
+	 }
+
+	 todos = append(todos, newTodo)
+}
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+func main() {
+	router := gin.Default()         // Create server
+	router.GET("/todos", getTodos)  // Create endpoint
+	router.Run("localhost:9090")    // Run server
 }
